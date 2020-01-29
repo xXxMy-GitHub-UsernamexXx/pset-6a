462503ff4a0300f270c442649834cab34ca52eec
Ethan Charles <ECharles1@ucvts.tec.nj.us>
2020-01-29 11:55:02 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const list = [];
+
+window.onload = function() {
+        document.getElementById("add button").onclick = addItem;
+
+};
+
+const addItem = function() {
+        let newItem = {
+                "description": "",
+                "priority": "low",
+                "completion": "incomplete"
+        };
+
+        let input = document.querySelector(".add-an-item")
+
+        newItem["description"] =
+
+};
